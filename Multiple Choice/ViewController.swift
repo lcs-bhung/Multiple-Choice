78af01ed32a7010edbb731f5f1498ec7f71e420f
Branton Hung <BHung@lcs.on.ca>
2019-12-03 14:59:04 -0500

    Initial Commit

---
+++ Multiple Choice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Choice
+//
+//  Created by Branton Hung on 2019-12-03.
+//  Copyright © 2019 Branton Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
