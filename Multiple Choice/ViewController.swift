9d9e243887113e1675cd976be18fbd3fe46eaf03
Branton Hung <BHung@lcs.on.ca>
2019-12-05 14:35:58 -0500

    Guard statements work

--- Multiple Choice/ViewController.swift
+++ Multiple Choice/ViewController.swift
@@ -14,7 +14,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var numberOfQuestions: UITextField!
+    @IBOutlet weak var studentAnswers: UITextField!
+    @IBOutlet weak var correctAnswers: UITextField!
+    @IBOutlet weak var result: UITextView!
+    
+    
+    @IBAction func checkAnswers(_ sender: Any) {
+        //Use guard statements
+        guard let numberOfQuesionsAsString = numberOfQuestions.text, numberOfQuesionsAsString != "" else {
+            result.text = "please enter an integer value greater than 0 "
+       return
+        }
+        guard let studentAnswers = studentAnswers.text, studentAnswers != "" else {
+            result.text = "please be sure you imput exactly \(numberOfQuesionsAsString) student answers"
+            return
+        }
+        guard let correctAnswers = correctAnswers.text, correctAnswers != "" else {
+            result.text = "Please be sure you enter exactly \(numberOfQuesionsAsString) answers for the answer key"
+            return
+        }
+        
+        
+        
+    }
+    
+    
 
 }
 
