4a0c13c361566b3f268699c6fbcdd3317bef0a1c
Branton Hung <BHung@lcs.on.ca>
2019-12-05 16:01:31 -0500

    Finished code

--- Multiple Choice/ViewController.swift
+++ Multiple Choice/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         //Use guard statements
         guard let numberOfQuesionsAsString = numberOfQuestions.text, numberOfQuesionsAsString != "" else {
             result.text = "please enter an integer value greater than 0 "
-       return
+            return
         }
         guard let studentAnswers = studentAnswers.text, studentAnswers != "" else {
             result.text = "please be sure you imput exactly \(numberOfQuesionsAsString) student answers"
@@ -34,12 +34,35 @@ class ViewController: UIViewController {
             result.text = "Please be sure you enter exactly \(numberOfQuesionsAsString) answers for the answer key"
             return
         }
+        var offset = 0
+        //make offset variable to determine the character location
+        var score = 0
+        //Determine the score
+        for character in studentAnswers {
+            //Get the first answer in the student answers
+            let indexStudent = studentAnswers.index(studentAnswers.startIndex, offsetBy: offset)
+            let studentAnswer = studentAnswers[indexStudent]
+            //Get the first correct answer
+            let indexAnswer = correctAnswers.index(correctAnswers.startIndex, offsetBy: offset)
+            let correctAnswer = correctAnswers[indexAnswer]
+            
+            if studentAnswer == correctAnswer {
+                score += 1
+            }
+            
+            offset += 1
+            
+            
+            result.text = "The student answered \(score) question(s) correctly"
+        }
+        
+        
         
         
         
     }
     
     
-
+    
 }
 
